38247645f928ab329ed317f870b88125f0634054
zealoustiger <zealoustiger@gmail.com>
2021-01-09 11:54:31 -0800

    heroku post-build script and hook up index.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const stocks = require('./routes/api/stocks');
 const app = express();
@@ -20,6 +21,15 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
   .then(() => console.log('Mongo Connected...'))
   .catch(err => console.log(err));
+
+// Serve static assets if in prod
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res)) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  }
+}
 
 // Use Routes
 app.use('/api/stocks', stocks)
